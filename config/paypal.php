1f01561de956ca829e8f609e985b64e87ad01f3e
yagi3mei <141975956+yagi3mei@users.noreply.github.com>
2025-12-16 17:13:21 +0800

    Adjust paypal config for one-time payment (local fix)

--- config/paypal.php
+++ config/paypal.php
@@ -9,7 +9,7 @@ return [
 
     'sandbox' => [
         'client_id' => env('PAYPAL_CLIENT_ID', ''),
-        'client_secret' => env('PAYPAL_CLIENT_SECRET', ''),
+        'client_secret' => env('PAYPAL_SECRET', ''),
         'app_id' => '',
     ],
 
